226cc9b7ea7554dfa81b09bfa0e09b0c34c88b1d
SyomaKiss <s.kiselev@innopolis.ru>
2018-03-04 21:15:57 +0300

    upd FrontEnd

--- lybrary/src/main/java/controllers/ActionController.java
+++ lybrary/src/main/java/controllers/ActionController.java
@@ -25,21 +25,45 @@ public class ActionController extends controllers.Controller {
             throws SQLException{
 
         String div = "<div id=settings_block>" +
-                "<div id=settings_type_menu>" +
-                "  <div class=settings_type id=settings_type_profile>Profile</div>" +
-                "  <div class=settings_type id=settings_type_users>Users</div>" +
-                "  <div class=settings_type id=settings_type_orders>Orders</div>" +
-                "  <div id=settings_type_line></div>" +
-                "</div>" +
-                "<div id=settings_profile" +
-                "<p class=setting_parameter_name><b>Change name</b></p>" +
-                "<input type=text class=settings_input id=settings_name placeholder=\"New name\" style=\"margin-top:-8px\" />" +
-                "<input type=text class=settings_input id=settings_surname placeholder=\"New surname\" />" +
-                "<p class=setting_parameter_name style=\"margin-top:16px\"><b>Change password</b></p>" +
-                "<input type=text class=settings_input id=settings_current_password placeholder=\"Current password\" style=\"margin-top:-8px\" />" +
-                "<input type=text class=settings_input id=settings_new_password placeholder=\"New password\" />" +
-                "<div id=settings_profile_save>Save</div>" +
-                "</div>" +
+                        "<div id=settings_type_menu>" +
+                        "<div class=settings_type id=settings_type_profile>Profile</div>" +
+                        "<div class=settings_type id=settings_type_users>Users</div>" +
+                        "<div class=settings_type id=settings_type_orders>Orders</div>" +
+                        "<div id=settings_type_line></div>" +
+                    "</div>" +
+                    "<div class=settings_type_box id=settings_profile>" +
+                        "<p class=setting_parameter_name><b>Change name</b></p>" +
+                        "<input type=text class=settings_input id=settings_name placeholder=\"New name\" style=\"margin-top:-8px\" />" +
+                        "<input type=text class=settings_input id=settings_surname placeholder=\"New surname\" />" +
+                        "<p class=setting_parameter_name style=\"margin-top:16px\"><b>Change password</b></p>" +
+                        "<input type=text class=settings_input id=settings_current_password placeholder=\"Current password\" style=\"margin-top:-8px\" />" +
+                        "<input type=text class=settings_input id=settings_new_password placeholder=\"New password\" />" +
+                        "<div id=settings_profile_save>Save</div>" +
+                    "</div>" +
+                    "<div class=settings_type_box id=settings_users>" +
+				    "<div id=new_user>+ Add a new user</div>" +
+				        "<div class=settings_list_users>" +
+					        "<img src=1.jpg width=100px height=100px class=settings_users_list_avatar />" +
+					        "<div class=settings_users_list_specs_box>" +
+						        "<b style=\"text-decoration:underline;\">Ilia Pro</b></br>" +
+						        "<b>Status:</b> Pizdat</br>" +
+						        "<b>Chlen:</b> Bolshoi" +
+                            "</div>" +
+					        "<div class=settings_users_list_modify id=228>Modify</div>" +
+				        "</div>" +
+			        "</div>" +
+                    "<div class=settings_type_box id=settings_orders>" +
+                        "<div id=new_book>+ Add a new book</div>" +
+                        "<div class=settings_list_orders>" +
+                            "<img src=1.jpg width=100px height=100px class=settings_orders_list_avatar />" +
+                            "<div class=settings_orders_list_specs_box>" +
+                                "<b style=\"text-decoration:underline;\">3 pigs</b></br>" +
+                                "#book #chtenie #hashtag</br>" +
+                                "<b>script:</b> Zaebis" +
+                            "</div>" +
+                            "<div class=settings_orders_list_modify id=228>Modify</div>" +
+                        "</div>" +
+                    "</div>" +
                 "</div>";
     
         return div;
